041c60adce9dd844f9b7783d2cb34f2882ad77f4
salt4172crp <wangyan4174@163.com>
2019-06-14 17:34:00 +0800

    person class for array

---
+++ src/com/crpower/ArrayPerson.java
@@ -0,0 +1,11 @@
+package com.crpower;
+
+public class ArrayPerson {
+    public static void main(String[] args) {
+        Person[] personArray = {new Person("wangan", 12), new Person("wans", 2)};
+        for (Person e : personArray) {
+            System.out.println(e);
+        }
+    }
+
+}
