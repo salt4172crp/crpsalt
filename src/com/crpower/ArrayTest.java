8c63d8c6781c287a548a086979c7a257d66a01b8
salt4172crp <wangyan4174@163.com>
2019-06-14 16:51:48 +0800

    with for loop,using index and length

--- src/com/crpower/ArrayTest.java
+++ src/com/crpower/ArrayTest.java
@@ -4,11 +4,13 @@ public class ArrayTest {
     public static void main(String[] args) {
         int[] arrayName;
         arrayName = new int[]{1, 2, 32, 310, -29};
-        for (int e : arrayName) {
-            // foreach loop ,elementary should be initial with type
-            System.out.println(e);
+//        for (int e : arrayName) {
+//            // foreach loop ,elementary should be initial with type
+//            System.out.println(e);
+//        }
+        for (int i = 0; i < arrayName.length; i++) {
+            System.out.println(arrayName[i]);
         }
-        
     }
 
 }
