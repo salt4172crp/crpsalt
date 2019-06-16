33e4e2a460056c9784f5fed581874d86e1c7f7e6
salt4172crp <wangyan4174@163.com>
2019-06-16 17:51:51 +0800

    array int initial

---
+++ src/com/crpower/ArrayInt.java
@@ -0,0 +1,11 @@
+package com.crpower;
+
+public class ArrayInt {
+    public static void main(String[] args) {
+        int[] arrInt;
+        arrInt = new int[]{1, 2, 3, 43, 10};
+        for (int i : arrInt) {
+            System.out.println(i);
+        }
+    }
+}
--- src/com/crpower/ArrayPerson.java
+++ src/com/crpower/ArrayPerson.java
@@ -4,7 +4,7 @@ public class ArrayPerson {
     public static void main(String[] args) {
         Person[] personArray = {new Person("wangan", 12), new Person("wans", 2)};
         for (Person e : personArray) {
-            System.out.println(e);
+            System.out.println(e.getAge() + " " + e.getName());
         }
     }
 
